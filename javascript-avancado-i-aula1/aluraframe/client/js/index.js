d301d7f7ccd1b7cc7f0c58a5114b886f4563c57e
Suzi Lima <suzileia.lima@minutrade.com>
2020-01-07 15:26:22 -0300

    adicionando js

---
+++ javascript-avancado-i-aula1/aluraframe/client/js/index.js
@@ -0,0 +1,46 @@
+var campos = [
+    document.querySelector('#data');
+document.querySelector('#quatidade');
+document.querySelector('#valor');
+];
+console.log(campos);
+
+document.querySelector('.form').addEventListener('sbmit', function (event) {
+    alert('Teçekuler')
+});
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+    var tr = document.createElement('tr');
+    campos.forEach(function (campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+
+        var tdVolume = document.createElement('td');
+        tdVolume.textContent = campos[1].value * campos[2].value;
+        tr.appendChild(tdVolume);
+        tbody.appendChild(tr);
+
+        campos[0].value = '';
+        campos[1].value = 1;
+        campos[2].value = 0;
+
+        campos[0].focus();
+
+    });
+
+});
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+    var tr = document.createElement(tr);
+
+    campos.forEach(function (campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+});
+});
+
