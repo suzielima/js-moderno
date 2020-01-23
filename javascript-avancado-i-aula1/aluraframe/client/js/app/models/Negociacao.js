f466c39c82ead850d2e8e2823a1c62ba46ed25fb
Suzi Lima <suzileia.lima@minutrade.com>
2020-01-23 16:45:35 -0300

    ajustes no código

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,12 +1,28 @@
 class Negociacao {
-
-  constructor(data, quantidade, valor) {
-    this.data = data;
-    this.quantidade = quantidade;
-    this.valor = valor;
-
-  }
-  obtemVolume() {
-    return this.quantidade * this.valor;
-  }
+    
+    constructor(data, quantidade, valor) {
+        
+        
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+    
+    get volume() {
+        
+        return this._quantidade * this._valor;
+    }
+    
+    get data() {
+        return new Date(this._data.getTime());
+    }
+    
+    get quantidade() {
+        return this._quantidade;
+    }
+    
+    get valor() {
+        return this._valor;
+    }
 }
