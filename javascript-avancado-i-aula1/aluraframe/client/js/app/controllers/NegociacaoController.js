804d27cfe043d43ad19f5247036895ff6415a213
Suzi Lima <suzileia.lima@minutrade.com>
2020-02-06 14:21:38 -0300

    criando outras classes

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -13,19 +13,45 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
-    let data = new Date(...this._inputData.value
-      .split('-')
-      .map((item, indice) => item - indice % 2)
-    );
-
-    console.log(data);
+    let helper = new DateHelper();
 
     let negociacao = new Negociacao(
-      data,
+      helper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
     );
-      console.log(negociacao);
+    console.log(negociacao);
+
+    console.log(helper.dataParaTexto(negociacao.data));
+
+
+
+
+
+
+
+    // let data = new Date(...this._inputData.value
+    //   .split('-')
+    //   .map((item, indice) => item - indice % 2)
+    // );
+
+    // console.log(data);
+
+    // let negociacao = new Negociacao(
+    //   data,
+    //   this._inputQuantidade.value,
+    //   this._inputValor.value
+    // );
+    // console.log(negociacao.data);
+
+    // //para pegar a data no padrao brasileiro
+
+    // let diaMesAno = negociacao.data.getDate() + '/' + (negociacao.data.getMonth() +1) + '/' + negociacao.data.getFullYear();
+    // console.log(diaMesAno);
+
+    //esse +1 adicionado no getMonth é para a contagem do mês, pois o array do ano veio de 0 a 11 (o 1 adicionar o mês exato que queremos)
+    //lembrando que operações precisam ser chamadas primeiro antes da concatenação, apenas adicionando entre () o que deseja ser operado primeiro.
+
 
     //adicionar essa negociação em uma lista
 
---
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,12 @@
+class DateHelper {
+
+  dataParaTexto(data) {
+
+    return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+  }
+  textoParaData(texto) {
+    return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+
+  }
+
+}
