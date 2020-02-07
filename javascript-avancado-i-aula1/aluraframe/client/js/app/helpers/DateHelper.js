e382fb86f5496efea4e603aabb7f53be5db92af9
Suzi Lima <suzileia.lima@minutrade.com>
2020-02-07 18:20:21 -0300

    template string e expressao regular

--- javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,10 +1,20 @@
 class DateHelper {
 
-  dataParaTexto(data) {
-
-    return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+  constructor() {
+    throw new Error('Esta classe não pode ser instanciada');
   }
-  textoParaData(texto) {
+
+  static dataParaTexto(data) {
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+  }
+  // return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();>> convertemos essa linha para template string acima dessa
+
+  static textoParaData(texto) {
+
+    if (!/\d{4}-\d{2}-\d{2}/.test(texto));
+
+    throw new Error('Deve estar no formato aaaa-mm-dd');
+
     return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
 
   }
