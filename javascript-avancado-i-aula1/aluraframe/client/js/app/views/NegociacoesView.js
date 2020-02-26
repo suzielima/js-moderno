c8e0f9507d926fa6f246de234533eeea994927ad
Suzi Lima <suzileia.lima@minutrade.com>
2020-02-26 17:24:15 -0300

    transformando as negociacoes

--- javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -18,9 +18,22 @@ class NegociacoesView {
        </thead>
 
        <tbody>
+        ${model.negociacoes.map(n => `
+
+            <tr>
+              <td>${DateHelper.dataParaTexto(n.data)}</td>
+              <td>${n.quatidade}</td>
+              <td>${n.valor}</td>
+              <td>${n.volume}</td>
+            </tr>
+          `).join('')}
        </tbody>
 
        <tfoot>
+          <td colspan = "3"></td>
+          <td>
+               ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+          </td>
        </tfoot>
    </table>
    `;
