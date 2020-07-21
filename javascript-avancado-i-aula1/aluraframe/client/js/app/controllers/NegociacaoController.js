5211424bee42c5e0ff1efb6d6060980efbc5d60d
Suzi Lima <suzileia.lima@minutrade.com>
2020-07-21 18:03:01 -0300

    modulo 2

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,57 +7,78 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    let self = this;
+    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
 
-  //   this._listaNegociacoes = new ListaNegociacoes(model => {
-  //     this._negociacoesView.update(model);
-  // });
+      get(target, prop, receiver) {
+
+        if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) == typeof (Function)) {
+
+          return function () {
+
+            console.log(`método '${prop}' interceptado`);
+
+            Reflect.apply(target[prop], target, arguments);
+
+            self._negociacoesView.update(target);
+
+          }
+        }
+
+        return Reflect.get(target, prop, receiver);
+      }
+    });
+
+    //   this._listaNegociacoes = new ListaNegociacoes(model => {
+    //     this._negociacoesView.update(model);
+    // });
 
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-//_indicação para o programador q ele não pode alterar negociações.
-this._negociacoesView.update(this._listaNegociacoes);
-this._mensagem = new Mensagem();
-this._mensagemView = new MensagemView($('#mensagemView'));
-this._mensagemView.update(this._mensagem);
+    //_indicação para o programador q ele não pode alterar negociações.
+    this._negociacoesView.update(this._listaNegociacoes);
+    this._mensagem = new Mensagem();
+    this._mensagemView = new MensagemView($('#mensagemView'));
+    this._mensagemView.update(this._mensagem);
   }
 
-adiciona(event) {
-  event.preventDefault();
+  adiciona(event) {
+    event.preventDefault();
 
-  this._listaNegociacoes.adiciona(this._criaNegociacao());
-  this._negociacoesView.update(this._listaNegociacoes);
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._negociacoesView.update(this._listaNegociacoes);
 
-  this._mensagem.texto = 'Negociação adicionada com sucesso';
-  this._mensagemView.update(this._mensagem);
+    this._mensagem.texto = 'Negociação adicionada com sucesso';
+    this._mensagemView.update(this._mensagem);
 
-  this._limpaFormulario();
-}
+    this._limpaFormulario();
+  }
 
-_criaNegociacao() {
+  _criaNegociacao() {
 
-  return new Negociacao(
-    DateHelper.textoParaData(this._inputData.value),
-    this._inputQuantidade.value,
-    this._inputValor.value
-  );
-}
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+  }
 
-_limpaFormulario() {
+  _limpaFormulario() {
 
-  this._inputData.value = '';
-  this._inputQuantidade.value = 1;
-  this._inputValor.value = 0.0;
-  this._inputData.focus();
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
 
 
-}
-apaga() {
-  this._listaNegociacoes.esvazia();
-  //this._negociacoesView.update(this._listaNegociacoes);
+  }
+  apaga() {
+    this._listaNegociacoes.esvazia();
+    //this._negociacoesView.update(this._listaNegociacoes);
 
-  this._mensagem.texto = 'Negociações apagadas com sucesso!';
-  this._mensagemView.update(this._mensagem);
+    this._mensagem.texto = 'Negociações apagadas com sucesso!';
+    this._mensagemView.update(this._mensagem);
 
-}
+  }
 }
 
 
